beee6e0a8d914932786af9b6560c4322b840b5c1
罗莹 <wb-ly520210@alibaba-inc.com>
2020-04-29 14:37:37 +0800

    测试用例和自动化构建

---
+++ hello.test.js
@@ -0,0 +1,9 @@
+var add = require('./hello.js');
+var expect = require('chai').expect;
+
+describe('hello', function() {
+  it('输出hello', function() {
+    expect(hello()).to.be.equal("Hello Node");
+  });
+});
+
